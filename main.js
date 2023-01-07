db98ef7ac7d394dc23fd9fea0bb399eb54eeb059
Felipe Pereira Martins <110059790+Felipe-Pereira-Martins@users.noreply.github.com>
2023-01-07 16:26:25 -0300

    Trabalhando com a const, lista de teclas

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 
 }
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-    document.querySelectorAll('.tecla');
+    //Refêrencias
+    
+    const listaDeTeclas = document.querySelectorAll('.tecla');
