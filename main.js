2d2800734ecb23d10a113b801e627374db19f100
Felipe Pereira Martins <110059790+Felipe-Pereira-Martins@users.noreply.github.com>
2023-01-07 16:42:01 -0300

    Trabalhando com lista de teclas

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function tocaSomPom () {
+    function tocaSomPom () {
 
     document.querySelector('#som_tecla_pom').play();
 
-}
+    }
+
     //Refêrencias
-    
     const listaDeTeclas = document.querySelectorAll('.tecla');
+
+    //Tocando som POM
+    listaDeTeclas[0].onclick = tocaSomPom;
